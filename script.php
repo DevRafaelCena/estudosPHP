3b108d279fe00b494ca793806fba926ccb212601
Rafael Cena <rafael.cena@hotmail.com>
2021-08-22 17:08:19 -0300

    inicio dos estudos

---
+++ script.php
@@ -0,0 +1,26 @@
+<?php
+
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+
+    
+    //validações
+    if(empty($name) or empty($age)){
+        echo 'os campos devem ser preenchidos.';
+        return;
+    }
+    if(strlen($name)<3 or strlen($name) >15){
+        echo 'nome deve conter mais de 3 caracterese e até 15 caracteres';
+        return;
+    }
+
+    if(!is_numeric($age)){
+        echo 'Digite apenas numero no campo idade';
+        return;
+    }
+
+
+
+
+?>
+
